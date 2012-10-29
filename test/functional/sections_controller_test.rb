a5ce89cfe2ad97b35e437c24c82add11b799e0ea
Benjamin Martin <benjamin247365@hotmail.com>
2012-10-29 23:09:05 +0000

    made tests run for no particular reason

--- test/functional/sections_controller_test.rb
+++ test/functional/sections_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SectionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
 end
