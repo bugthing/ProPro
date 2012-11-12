ab3b303d5f483ee2b96ae524d713c844bc07f94a
Benjamin Martin <benjamin247365@hotmail.com>
2012-11-12 23:05:17 +0000

    tweaked and tidy router alittle

--- app/assets/javascripts/app/routes/app_router.js
+++ app/assets/javascripts/app/routes/app_router.js
@@ -1,106 +1,103 @@
 App.Router = Ember.Router.extend({
   enableLogging: true,
   location: 'hash',
-  
   root: Em.Route.extend({
     charts: Em.Route.extend({
       route: '/',
+      connectOutlets: function(router) {
+        router.get('applicationController').connectOutlet('charts', router.get('store').findAll(App.Chart));
+      },
+      index: Em.Route.extend({
+        route: '/'
+      }),
       
       showChart: function(router, event) {
         router.transitionTo('charts.chart.index', event.context);
       },
-
       showNewChart: function(router) {
         router.transitionTo('charts.newChart', {});
       },
 
-      connectOutlets: function(router) {
-        router.get('applicationController').connectOutlet('charts', router.get('store').findAll(App.Chart));
-      },
-
-      index: Em.Route.extend({
-        route: '/',
-
-        connectOutlets: function(router) {
-          router.get('applicationController').connectOutlet('charts');
-        }
-      }),
-
       newChart: Em.Route.extend({
-        route: '/charts/new',
-
-        cancelEdit: function(router) {
-          router.transitionTo('charts.index');
-        },
-
+        route: 'charts/new',
         connectOutlets: function(router) {
           router.get('chartsController').connectOutlet('editChart', {});
           router.get('editChartController').enterEditing();
         },
 
+        cancelEdit: function(router) {
+          router.transitionTo('charts.index');
+        },
         exit: function(router) {
           router.get('editChartController').exitEditing();
         }
       }),
-
       chart: Em.Route.extend({
-        route: '/charts/:chart_id',
-
+        route: 'charts/:chart_id',
         connectOutlets: function(router, context) {
           router.get('chartsController').connectOutlet('chart', context);
         },
 
         index: Em.Route.extend({
           route: '/',
+          connectOutlets: function(router, context) {
+            router.get('chartController').connectOutlet('showChart');
+          },
 
           showEdit: function(router) {
             router.transitionTo('charts.chart.edit');
           },
-
           showBuild: function(router) {
             router.transitionTo('charts.chart.build');
           },
-
-          connectOutlets: function(router, context) {
-            router.get('chartController').connectOutlet('showChart');
-          }
         }),
-
-        build: Em.Route.extend({
-          route: 'build',
-
-          connectOutlets: function(router) {
-            var chartController = router.get('chartController');
-            chartController.connectOutlet('buildChart', chartController.get('content'));
-            router.get('buildChartController').enterBuilding();
-          },
-
-          exit: function(router) {
-            router.get('buildChartController').exitBuilding();
-          },
-
-          cancelBuild: function(router) {
-            router.transitionTo('charts.chart.index');
-          }
-
-        }),
-
         edit: Em.Route.extend({
           route: 'edit',
-
-          cancelEdit: function(router) {
-            router.transitionTo('charts.chart.index');
-          },
-
           connectOutlets: function(router) {
             var chartController = router.get('chartController');
             chartController.connectOutlet('editChart', chartController.get('content'));
             router.get('editChartController').enterEditing();
           },
 
+          cancelEdit: function(router) {
+            router.transitionTo('charts.chart.index');
+          },
           exit: function(router) {
             router.get('editChartController').exitEditing();
           }
+        }),
+        build: Em.Route.extend({
+          route: 'build',
+          connectOutlets: function(router) {
+            var chartController = router.get('chartController');
+            var chart = chartController.get('content');
+            //router.get('applicationController').connectOutlet('buildChart', chart);
+            chartController.connectOutlet('buildChart', chart);
+            router.get('buildChartController').enterBuilding();
+
+          },
+          index: Ember.Route.extend({
+            route: '/'
+          }),
+
+          exit: function(router) {
+            router.get('buildChartController').exitBuilding();
+          },
+          cancelBuild: function(router) {
+            router.transitionTo('charts.chart.index');
+          },
+          showSections: function(router, event) {
+            router.transitionTo('charts.chart.build.sections', event.context);
+          },
+          sections: Ember.Route.extend({
+            route: 'sections',
+            connectOutlets: function(router) {
+              //router.get('applicationController').connectOutlet('charts', router.get('store').findAll(App.Chart));
+            },
+            index: Em.Route.extend({
+              route: '/'
+            })
+          })
         })
       })
     })
