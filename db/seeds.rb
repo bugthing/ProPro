ab8f636d0ecf94061efc8f799695c685989f9253
Benjamin Martin <benjamin247365@hotmail.com>
2012-12-20 19:30:18 +0000

    fixed db seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 
 # Add a User group (required)
-UserGroup.create( name: 'User Group One' )
+user_group = UserGroup.create( name: 'User Group One' )
 
 # Add the default tools;
 ['Plain Text','Input Box','Next Button'].each do |tool_name|
-  Tool.create( name: tool_name )
+  user_group.tools.create( name: tool_name )
 end
 
